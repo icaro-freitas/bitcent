1d87e9309179a6dca91bc1ed3d1890495fe23187
icaro-freitas <icaro_freitas2014@outlook.com>
2023-05-12 23:03:22 -0300

    Foram desenvolvidos os componentes area,logo,Menu e MenuItem

--- src/components/landing/destaque/index.tsx
+++ src/components/landing/destaque/index.tsx
@@ -1,9 +1,15 @@
 import Area from "../comum/Area";
+import Slogan from "./Slogan";
+
 
 export default function Destaque() {
     return (
-        <Area id="inicio">
-                        
+        <Area id="inicio" className="pt-20">
+            <div className={`
+            flex items-center
+            h-[500px]`}>
+            <Slogan/>   
+            </div>
         </Area>
     )
 }
